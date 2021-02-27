d16c02f5e6ac8e8c6380ce2f5162bc45e26a0b89
Nameless <truemedian@gmail.com>
2021-02-27 12:07:25 -0600

    initial version

---
+++ package.lua
@@ -0,0 +1,13 @@
+return {
+    name = "truemedian/lit-glob",
+    version = "0.0.1",
+    description = "A file globbing library.",
+    tags = { "fs", "glob" },
+    license = "MIT",
+    author = { name = "Nameless" },
+    homepage = "https://github.com/truemedian/lit-glob",
+    dependencies = {},
+    files = {
+        "**.lua",
+    }
+}
